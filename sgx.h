c5353caf448fda672711ef10deffab1900ee1527
Your Name <you@example.com>
2021-01-02 13:24:27 +0800

    fortanix version

--- sgx.h
+++ sgx.h
@@ -160,6 +160,7 @@ enum sgx_encl_flags {
 
 struct sgx_encl {
 	unsigned int flags;
+	unsigned int id;
 	uint64_t attributes;
 	uint64_t xfrm;
 	unsigned int secs_child_cnt;
@@ -172,6 +173,7 @@ struct sgx_encl {
 	unsigned long base;
 	unsigned long size;
 	unsigned long ssaframesize;
+	unsigned long eadd_cnt;  /* XXX: what about removals? */
 	struct list_head va_pages;
 	struct radix_tree_root page_tree;
 	struct list_head add_page_reqs;
@@ -179,6 +181,7 @@ struct sgx_encl {
 	struct sgx_encl_page secs;
 	struct sgx_tgid_ctx *tgid_ctx;
 	struct list_head encl_list;
+	struct list_head all_list;
 	struct mmu_notifier mmu_notifier;
 	unsigned int shadow_epoch;
 };
@@ -200,6 +203,11 @@ extern u64 sgx_xfrm_mask;
 extern u32 sgx_misc_reserved;
 extern u32 sgx_xsave_size_tbl[64];
 extern bool sgx_has_sgx2;
+
+/* stats */
+extern unsigned int sgx_encl_created;
+extern unsigned int sgx_encl_released;
+extern long unsigned sgx_retired_eadd_cnt;
 
 extern const struct vm_operations_struct sgx_vm_ops;
 
@@ -264,6 +272,7 @@ struct sgx_encl_page *sgx_fault_page(struct vm_area_struct *vma,
 
 extern struct mutex sgx_tgid_ctx_mutex;
 extern struct list_head sgx_tgid_ctx_list;
+extern struct list_head sgx_all_encl_list;
 extern atomic_t sgx_va_pages_cnt;
 
 int sgx_add_epc_bank(resource_size_t start, unsigned long size, int bank);
@@ -282,4 +291,10 @@ long modify_range(struct sgx_range *rg, unsigned long flags);
 int remove_page(struct sgx_encl *encl, unsigned long address, bool trim);
 int sgx_get_encl(unsigned long addr, struct sgx_encl **encl);
 int sgx_vm_insert_pfn(struct vm_area_struct *vma, unsigned long addr,  resource_size_t pa);
+int sgx_stats_read(struct seq_file *file, void *v);
+void *sgx_encl_seq_start(struct seq_file *seq, loff_t *pos);
+void *sgx_encl_seq_next(struct seq_file *seq, void *v, loff_t *pos);
+void sgx_encl_seq_stop(struct seq_file *seq, void *v);
+int sgx_encl_seq_show(struct seq_file *file, void *v);
+
 #endif /* __ARCH_X86_INTEL_SGX_H__ */
